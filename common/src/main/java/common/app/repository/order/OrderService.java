09914de50d06b5e00d2deed376f3dcc7ed0cf20a
kaikun213 <kaktus.w.h@gmx.de>
2017-04-07 16:24:41 +0200

    Uncommented Spring Data
    Spring Data needs JPA Metamodel defined

--- common/src/main/java/common/app/repository/order/OrderService.java
+++ common/src/main/java/common/app/repository/order/OrderService.java
@@ -1,21 +1,21 @@
-package common.app.repository.order;
-
-import java.util.List;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import common.app.model.merchant.Merchant;
-import common.app.model.order.Order;
-import common.app.model.user.User;
-
-/**
- * This Service provides the necessary functionality to add finalized transactions
- * @author kaikun
- *
- */
-public interface OrderService extends PagingAndSortingRepository<Order,Long>{
-	
-	List<Order> findByUser(User user);
-	List<Order> findByMerchant(Merchant merchant);
-
-}
+//package common.app.repository.order;
+//
+//import java.util.List;
+//
+//import org.springframework.data.repository.PagingAndSortingRepository;
+//
+//import common.app.model.merchant.Merchant;
+//import common.app.model.order.Order;
+//import common.app.model.user.User;
+//
+///**
+// * This Service provides the necessary functionality to add finalized transactions
+// * @author kaikun
+// *
+// */
+//public interface OrderService extends PagingAndSortingRepository<Order,Long>{
+//	
+//	List<Order> findByUser(User user);
+//	List<Order> findByMerchant(Merchant merchant);
+//
+//}
--- transaction/src/main/java/com/smarter_transfer/springrest/transaction/web/OrderResource.java
+++ transaction/src/main/java/com/smarter_transfer/springrest/transaction/web/OrderResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import common.app.model.order.Order;
-import common.app.repository.order.OrderService;
+//import common.app.repository.order.OrderService;
 import common.app.web.ApiResponse;
 import common.app.web.ApiResponse.ApiError;
 import common.app.web.ApiResponse.Status;
@@ -17,20 +17,20 @@ import common.app.web.ApiResponse.Status;
 @RequestMapping("/orders")
 public class OrderResource {
 	
-	@Autowired
-	OrderService orderService;
-	
-	@RequestMapping(value="/{userId}", method=RequestMethod.POST, consumes = "application/json")
-	public ApiResponse addItem(@PathVariable long userId,@RequestBody OrderDTO orderDTO){
-		try{
-			Order newOrder = createOrder(userId, orderDTO);
-			orderService.save(newOrder);
-			return new ApiResponse(Status.OK,new OrderDTO(newOrder), null);
-		}
-		catch (Exception e){
-            return new ApiResponse(Status.ERROR, null, new ApiError(400, e.getMessage()));
-		}
-	}
+//	@Autowired
+//	OrderService orderService;
+//	
+//	@RequestMapping(value="/{userId}", method=RequestMethod.POST, consumes = "application/json")
+//	public ApiResponse addItem(@PathVariable long userId,@RequestBody OrderDTO orderDTO){
+//		try{
+//			Order newOrder = createOrder(userId, orderDTO);
+//			orderService.save(newOrder);
+//			return new ApiResponse(Status.OK,new OrderDTO(newOrder), null);
+//		}
+//		catch (Exception e){
+//            return new ApiResponse(Status.ERROR, null, new ApiError(400, e.getMessage()));
+//		}
+//	}
 	
 	private Order createOrder(long userId, OrderDTO orderDTO){
 		// TODO write order object (look item create)
